c75700d286b33c51028cc4a8ecfd85de61bf3485
Edmilson-Filimone <75740869+Edmilson-Filimone@users.noreply.github.com>
2022-12-21 09:56:10 +0200

    Add files via upload

--- danfo_script.js
+++ danfo_script.js
@@ -2,17 +2,45 @@
 let dados = { nome: [], canister: [], caixa: [], posicao: [], data: [] };
 
 //o dataframe
-let dataframe
+let dataframe;
 
-//pegando o formulario e o botao
+//pegando o formulario, botao, e a notificacao
+const body = document.querySelector("body");
 const form = document.querySelector("#form");
 const submit = document.querySelector("#submit");
-const download = document.querySelector('#btn-download')
+const download = document.querySelector("#btn-download");
+const notification = document.querySelector(".card-conteiner");
+const showRecords = document.querySelector(".btn-see-more");
+const icon_btn = document.querySelector('.fa-plus')
+
+/*Criando elementos para a lista de registros*/
+let painel = document.createElement("div"); //div para mostrar os resultados
+let placeholder = document.createElement("p");
+placeholder.classList.add('placeholder-style')
+placeholder.innerText = "Ainda não existem registros!"
+painel.append(placeholder)
+
+let list = document.createElement("ul");
+list.classList.add("list-style")
+painel.classList.add("display-none");
+painel.append(list);
+body.append(painel);
+
+//Painel de vizualicao de registro
+function savePainel() {
+  painel.removeChild(placeholder)
+  let li = document.createElement("li");
+
+  let data_iterate = dfd.toJSON(dataframe);
+  for (let item of data_iterate) {
+    li.innerText = `Nome: ${item.nome} - Posicao: C${item.canister}-C${item.caixa}-P${item.posicao}`;
+    list.append(li);
+  }
+}
 
 //Funcao para salvar e guardar como planilha
-function save_data() {
-  
- //dom
+function saveData() {
+  //dom
   let nome = document.querySelector("#nome");
   let canister = document.querySelector("#canister");
   let caixa = document.querySelector("#caixa");
@@ -25,25 +53,36 @@ function save_data() {
   dados.caixa.push(caixa.value);
   dados.posicao.push(posicao.value);
   dados.data.push(data.value);
-  
+
   /*convertendo o objecto em dataframe*/
-  dataframe = new dfd.DataFrame(dados)
-  dataframe.print()
-
-  console.log(dfd.toJSON(dataframe))
-
+  dataframe = new dfd.DataFrame(dados);
+  console.log(dfd.toJSON(dataframe)); //transforma o df em um array de objectos
 }
 
+/*Funcao para o display da notificacao*/
+function showAlert() {
+  notification.style.display = "flex";
+  setTimeout(() => {
+    notification.style.display = "none";
+  }, 2000);
+}
 
 /*Eventos*/
 form.addEventListener("submit", (e) => {
   e.preventDefault();
-  save_data();
+  saveData();
   form.reset();
+  showAlert();
+  savePainel();
   console.log(dados);
 });
 
 download.addEventListener("click", (e) => {
-    e.preventDefault()
-    dfd.toCSV(dataframe, { fileName: "Planilha_criobak.csv", download: true });
-})
+  dfd.toCSV(dataframe, { fileName: "Planilha_criobank.csv", download: true });
+});
+
+showRecords.addEventListener("click", () => {
+  icon_btn.classList.toggle("rotate");
+  painel.classList.toggle('painel-style');
+  painel.classList.toggle('display-none');
+});
